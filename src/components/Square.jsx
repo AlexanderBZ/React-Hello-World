2c11da278ce651cc52659c03c77441db5305f116
Alexander Zwerner <Alexander.zwerner@gmail.com>
2024-11-13 09:02:50 -0600

    replace value for state

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { useState } from "react";
 
-function Square({ value }) {
+function Square() {
+  const [value, setValue] = useState(null);
+
   function handleClick() {
-    console.log("clicked!");
+    setValue("X");
   }
 
   return (
